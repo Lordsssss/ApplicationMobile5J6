516de891764edfaf1d109b7e3ec094516a97afa3
Lordsssss <202143450@cmontmorency.qc.ca>
2023-09-07 08:04:33 -0400

    Fait: Cacher/afficher mot de passe avec checkbox

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.lab1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText passwordEditText = findViewById(R.id.passwordEditText);
+        CheckBox showPasswordCheckBox = findViewById(R.id.showPasswordCheckBox);
+
+        showPasswordCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Pour afficher le mot de passe
+                    passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                } else {
+                    // Pour masquer le mot de passe
+                    passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+                // Pour garder le curseur à la fin du texte
+                passwordEditText.setSelection(passwordEditText.length());
+            }
+        });
     }
 }
